e9446590c773191f44f9438acd26abb34cba9b8f
Mgokayay <aymustafagokay@gmail.com>
2024-03-19 11:12:37 +0300

    entity created

---
+++ src/main/java/com/example/bankingapp/entity/Account.java
@@ -0,0 +1,23 @@
+package com.example.bankingapp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+
+    @Column(name = "balance")
+    private double balance;
+}
